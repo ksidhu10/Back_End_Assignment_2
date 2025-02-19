e03c390a163b85f574d0f87829909fd0ac46af97
rrc_w2024_kiran <ksidhu10@academic.rrc.ca>
2025-02-18 23:44:40 -0600

    Created custom error classes (AppError, ValidationError, NotFoundError) to handle different error types

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import express from 'express';
-import branchRoutes from './api/v1/routes/branchroutes';
-import setupSwagger from './swagger'; // Fixed incorrect import path
-import employeeRoutes from './api/v1/routes/employeeroutes';
+import branchRoutes from "./api/v1/routes/branchroutes";
+import setupSwagger from "../src/swagger";
+import employeeRoutes from "./api/v1/routes/employeeroutes";
 
 const app = express();
 
-app.use(express.json()); // Add middleware for JSON parsing
-app.use('/api/v1/employees', employeeRoutes); // Ensure the versioning is consistent
-app.use('/api/v1/branches', branchRoutes);
+app.use("/api/v1/employees", employeeRoutes);
+app.use("/api/v1/branches", branchRoutes);
 
 // Health check route
 app.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Server is healthy' });
+  res.status(200).json({ message: "Server is healthy" });  
 });
+
+
 
 export default app;
